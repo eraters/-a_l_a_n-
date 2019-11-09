0d2dfb22b27c98d3dc53a15b0bb2ad9694f793ea
imlazywithazismyname <54771596+imlazywithazismyname@users.noreply.github.com>
2019-11-08 21:16:39 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,10 +9,9 @@ from itertools import cycle
 client = commands.Bot(command_prefix = 'a!')
 
 @client.event
-await client.change_presence(status=discord.Status.Online, activity=discord.Game("~A_L_A_N~'S Discord bot (coded by ImLazyWithAZ#8327 "))
 async def on_ready():
   print('Bot is ready')
-  
+  await client.change_presence(status=discord.Status.Online, activity=discord.Game("~A_L_A_N~'S Discord bot (coded by ImLazyWithAZ#8327 "))
 @client.command()
 async def start(ctx):
   await ctx.send('This bot has been started')
