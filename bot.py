5c4b94d283a828c92a39bc74566a617452b0e60e
imlazywithazismyname <54771596+imlazywithazismyname@users.noreply.github.com>
2019-11-08 19:01:41 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,10 @@
 import discord
 from discord.ext import commands
 
-TOKEN = os.environ.get("TOKEN")
-
 client = commands.Bot(command_prefix = 'a!')
 
 @client.event
 async def on_ready():
   print('Bot is ready')
 
-client.run(TOKEN)
+client.run(os.getenv('TOKEN'))
